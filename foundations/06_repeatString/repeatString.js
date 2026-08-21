9c250fd97ca4b044afa4d642ac3f0d657e9d3e57
Ella Braun <ellafbraun@gmail.com>
2026-08-21 15:19:29 +0100

    second js exercises

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -2,7 +2,12 @@ const repeatString = function(string, num) {
     if (num < 0) {
         return 'ERROR';
     } else {
-        
+        let word = "";
+        for(i=0; i<num; i++) {
+            // concatenate the strings to each other this many times
+            word+=string;
+        }
+        return word;
     }
 
 };
--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removal) {
+    // helper function for filter()
+    function isRemoval(num) {
+        if (removal.includes(num)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+
+    const filtered = array.filter(isRemoval);
+    return filtered;
+
 };
 
 // Do not edit below this line
